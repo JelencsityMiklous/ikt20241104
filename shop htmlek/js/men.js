a66d08d34712a712afcd3ec133abdaa97ea5e29a
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-02-24 08:27:54 +0100

    IK-55 Javascript objektumok a termékekről alapjának elkészítése, casual oldal szerkezetének átmásolása a többi termék oldalra

---
+++ shop htmlek/js/men.js
@@ -0,0 +1,34 @@
+/* Objektum a termékekről */
+
+let termekek = [
+	{
+		name:"Collar T-shirt",
+		price:35,
+		gender:"men"
+	},
+	{
+		name:"Gents T-Shirt",
+		price:49,
+		gender:"men"
+	},
+	{
+		name:"Ladies Hat",
+		price:17,
+		gender:"women"
+	},
+	{
+		name:"Leather Jacket",
+		price:50,
+		gender:"men"
+	},
+	{
+		name:"Printed Tops",
+		price:75,
+		gender:"women"
+	},
+	{
+		name:"Women Tops",
+		price:45,
+		gender:"women"
+	}
+]
