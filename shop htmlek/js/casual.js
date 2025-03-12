fec37879945955143b4e483d4306a9311d4e77cf
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-03-12 08:44:15 +0100

    IK-61 Elkészült sorting implementálása a többi oldalra

--- shop htmlek/js/casual.js
+++ shop htmlek/js/casual.js
@@ -1,48 +1,4 @@
-const rangeSlider_min = 30;
-const rangeSlider_max = 60;
 
-document.querySelector('#RangeSlider .range-slider-val-left').style.width = `${rangeSlider_min + (100 - rangeSlider_max)}%`;
-document.querySelector('#RangeSlider .range-slider-val-right').style.width = `${rangeSlider_min + (100 - rangeSlider_max)}%`;
- 
-document.querySelector('#RangeSlider .range-slider-val-range').style.left = `${rangeSlider_min}%`;
-document.querySelector('#RangeSlider .range-slider-val-range').style.right = `${(100 - rangeSlider_max)}%`;
-
-document.querySelector('#RangeSlider .range-slider-handle-left').style.left = `${rangeSlider_min}%`;
-document.querySelector('#RangeSlider .range-slider-handle-right').style.left = `${rangeSlider_max}%`;
-
-document.querySelector('#RangeSlider .range-slider-tooltip-left').style.left = `${rangeSlider_min}%`;
-document.querySelector('#RangeSlider .range-slider-tooltip-right').style.left = `${rangeSlider_max}%`;
-
-document.querySelector('#RangeSlider .range-slider-tooltip-left .range-slider-tooltip-text').innerText = rangeSlider_min;
-document.querySelector('#RangeSlider .range-slider-tooltip-right .range-slider-tooltip-text').innerText = rangeSlider_max;
-
-document.querySelector('#RangeSlider .range-slider-input-left').value = rangeSlider_min;
-document.querySelector('#RangeSlider .range-slider-input-left').addEventListener( 'input', e => {
-	e.target.value = Math.min(e.target.value, e.target.parentNode.childNodes[5].value - 1);
-	var value = (100 / ( parseInt(e.target.max) - parseInt(e.target.min) )) * parseInt(e.target.value) - (100 / (parseInt(e.target.max) - parseInt(e.target.min) )) * parseInt(e.target.min);
-
-	var children = e.target.parentNode.childNodes[1].childNodes;
-	children[1].style.width = `${value}%`;
-	children[5].style.left = `${value}%`;
-	children[7].style.left = `${value}%`;
-	children[11].style.left = `${value}%`;
-
-	children[11].childNodes[1].innerHTML = e.target.value;
-});
-
-document.querySelector('#RangeSlider .range-slider-input-right').value = rangeSlider_max;
-document.querySelector('#RangeSlider .range-slider-input-right').addEventListener( 'input', e => {
-	e.target.value = Math.max(e.target.value, e.target.parentNode.childNodes[3].value - (-1));
-	var value = (100 / ( parseInt(e.target.max) - parseInt(e.target.min) )) * parseInt(e.target.value) - (100 / ( parseInt(e.target.max) - parseInt(e.target.min) )) * parseInt(e.target.min);
-
-	var children = e.target.parentNode.childNodes[1].childNodes;
-	children[3].style.width = `${100-value}%`;
-	children[5].style.right = `${100-value}%`;
-	children[9].style.left = `${value}%`;
-	children[13].style.left = `${value}%`;
-
-	children[13].childNodes[1].innerHTML = e.target.value;
-});
 
 let mybutton = document.getElementById("topgomb");
 
@@ -170,7 +126,6 @@ let eredeti_termekek = [
 ]
 
 
-let sortertek
 
 function Sorting(){
 
@@ -267,37 +222,37 @@ function Sorting(){
 			document.getElementById("elsoar").innerHTML=`$${termekek[0].price}.00`
 		}
 		else{
-			document.getElementById("elsoar").innerHTML=`$${termekek[0].discount_price} $${termekek[0].price}.00`
+			document.getElementById("elsoar").innerHTML=`$${termekek[0].discount_price}.00 $${termekek[0].price}.00`
 		}
 		if(termekek[1].discount_price==""){
 			document.getElementById("masodikar").innerHTML=`$${termekek[1].price}.00`
 		}
 		else{
-			document.getElementById("masodikar").innerHTML=`$${termekek[1].discount_price} $${termekek[1].price}.00`
+			document.getElementById("masodikar").innerHTML=`$${termekek[1].discount_price}.00 $${termekek[1].price}.00`
 		}
 		if(termekek[2].discount_price==""){
 			document.getElementById("harmadikar").innerHTML=`$${termekek[2].price}.00`
 		}
 		else{
-			document.getElementById("harmadikar").innerHTML=`$${termekek[2].discount_price} $${termekek[2].price}.00`
+			document.getElementById("harmadikar").innerHTML=`$${termekek[2].discount_price}.00 $${termekek[2].price}.00`
 		}
 		if(termekek[3].discount_price==""){
 			document.getElementById("negyedikar").innerHTML=`$${termekek[3].price}.00`
 		}
 		else{
-			document.getElementById("negyedikar").innerHTML=`$${termekek[3].discount_price} $${termekek[3].price}.00`
+			document.getElementById("negyedikar").innerHTML=`$${termekek[3].discount_price}.00 $${termekek[3].price}.00`
 		}
 		if(termekek[4].discount_price==""){
 			document.getElementById("otodikar").innerHTML=`$${termekek[4].price}.00`
 		}
 		else{
-			document.getElementById("otodikar").innerHTML=`$${termekek[4].discount_price} $${termekek[4].price}.00`
+			document.getElementById("otodikar").innerHTML=`$${termekek[4].discount_price}.00 $${termekek[4].price}.00`
 		}
 		if(termekek[5].discount_price==""){
 			document.getElementById("hatodikar").innerHTML=`$${termekek[5].price}.00`
 		}
 		else{
-			document.getElementById("hatodikar").innerHTML=`$${termekek[5].discount_price} $${termekek[5].price}.00`
+			document.getElementById("hatodikar").innerHTML=`$${termekek[5].discount_price}.00 $${termekek[5].price}.00`
 		}
 		
 		/*Terméknév*/
@@ -334,37 +289,37 @@ function Sorting(){
 			document.getElementById("elsoar").innerHTML=`$${termekek[0].price}.00`
 		}
 		else{
-			document.getElementById("elsoar").innerHTML=`$${termekek[0].discount_price} $${termekek[0].price}.00`
+			document.getElementById("elsoar").innerHTML=`$${termekek[0].discount_price}.00 $${termekek[0].price}.00`
 		}
 		if(termekek[1].discount_price==""){
 			document.getElementById("masodikar").innerHTML=`$${termekek[1].price}.00`
 		}
 		else{
-			document.getElementById("masodikar").innerHTML=`$${termekek[1].discount_price} $${termekek[1].price}.00`
+			document.getElementById("masodikar").innerHTML=`$${termekek[1].discount_price}.00 $${termekek[1].price}.00`
 		}
 		if(termekek[2].discount_price==""){
 			document.getElementById("harmadikar").innerHTML=`$${termekek[2].price}.00`
 		}
 		else{
-			document.getElementById("harmadikar").innerHTML=`$${termekek[2].discount_price} $${termekek[2].price}.00`
+			document.getElementById("harmadikar").innerHTML=`$${termekek[2].discount_price}.00 $${termekek[2].price}.00`
 		}
 		if(termekek[3].discount_price==""){
 			document.getElementById("negyedikar").innerHTML=`$${termekek[3].price}.00`
 		}
 		else{
-			document.getElementById("negyedikar").innerHTML=`$${termekek[3].discount_price} $${termekek[3].price}.00`
+			document.getElementById("negyedikar").innerHTML=`$${termekek[3].discount_price}.00 $${termekek[3].price}.00`
 		}
 		if(termekek[4].discount_price==""){
 			document.getElementById("otodikar").innerHTML=`$${termekek[4].price}.00`
 		}
 		else{
-			document.getElementById("otodikar").innerHTML=`$${termekek[4].discount_price} $${termekek[4].price}.00`
+			document.getElementById("otodikar").innerHTML=`$${termekek[4].discount_price}.00 $${termekek[4].price}.00`
 		}
 		if(termekek[5].discount_price==""){
 			document.getElementById("hatodikar").innerHTML=`$${termekek[5].price}.00`
 		}
 		else{
-			document.getElementById("hatodikar").innerHTML=`$${termekek[5].discount_price} $${termekek[5].price}.00`
+			document.getElementById("hatodikar").innerHTML=`$${termekek[5].discount_price}.00 $${termekek[5].price}.00`
 		}
 
 
--- shop htmlek/js/women.js
+++ shop htmlek/js/women.js
@@ -5,36 +5,48 @@ let termekek = [
 		name:"Denim Jeans",
 		price:35,
 		gender:"women",
-		discount:false
+		discount:false,
+		discount_price:"",
+		img:"../"
 	},
 	{
 		name:"Knitted Top",
 		price:49,
 		gender:"women",
-		discount:false
+		discount:false,
+		discount_price:"",
+		img:"../"
 	},
 	{
 		name:"White vest",
 		price:15,
 		gender:"women",
-		discount:true
+		discount:true,
+		discount_price:"",
+		img:"../"
 	},
 	{
 		name:"White Jeans",
 		price:45,
 		gender:"women",
-		discount:true
+		discount:true,
+		discount_price:"",
+		img:"../"
 	},
 	{
 		name:"White Shirt",
 		price:35,
 		gender:"women",
-		discount:false
+		discount:false,
+		discount_price:"",
+		img:"../"
 	},
 	{
 		name:"Flower Dress",
 		price:15,
 		gender:"women",
-		discount:false
+		discount:false,
+		discount_price:"",
+		img:"../"
 	}
 ]
