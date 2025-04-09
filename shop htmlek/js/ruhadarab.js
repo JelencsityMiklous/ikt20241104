fce1b9cc46f4202ef85fb7c51ff711bc39d4c3ad
OhradkaGergo <ohradkagergo@turr.hu>
2025-04-09 10:05:44 +0200

    IK-67 - Clothing ruhadarab oldal adatfeltöltés, casual oldal adatfeltöltés befejezése

--- shop htmlek/js/ruhadarab.js
+++ shop htmlek/js/ruhadarab.js
@@ -304,6 +304,7 @@ function feltoltAdat() {
 		main_image.src="../kepek/accessories/alpha_industries_sapka.png"
 	}
 
+	//casual termékek
 	else if (productSzam == 7) {
 		oldalcim.innerHTML = "Collar T-Shirt"
 		fejlec_cim.innerHTML = "Home / Products / Casual / Collar T-Shirt"
@@ -344,18 +345,315 @@ function feltoltAdat() {
 		let list_item10 = document.createElement("li")
 		list_item10.innerHTML = casual_termekek[0].meret_szabas1
 		let list_item11 = document.createElement("li")
-		list_item11.innerHTML = casual_termekek[0].meret_szabas1
+		list_item11.innerHTML = casual_termekek[0].meret_szabas2
 		let list_item12 = document.createElement("li")
-		list_item12.innerHTML = casual_termekek[0].meret_szabas1
+		list_item12.innerHTML = casual_termekek[0].meret_szabas3
 		let list_item13 = document.createElement("li")
-		list_item13.innerHTML = casual_termekek[0].meret_szabas1
+		list_item13.innerHTML = casual_termekek[0].meret_szabas4
 
 		un_list_meret.appendChild(list_item10)
 		un_list_meret.appendChild(list_item11)
 		un_list_meret.appendChild(list_item12)
 		un_list_meret.appendChild(list_item13)
 
-		main_image.src="../kepek/collartshirt.jpg"
+		main_image.src="../kepek/collar-light-gray-t-m.png"
+	}
+
+	else if (productSzam == 8) {
+		oldalcim.innerHTML = "Gents T-Shirt"
+		fejlec_cim.innerHTML = "Home / Products / Casual / Gents T-Shirt"
+
+		document.getElementById("product_nev").innerHTML = casual_termekek[1].nev;
+
+		document.getElementById("product_ar").innerHTML = casual_termekek[1].price;
+		
+		let list_item1 = document.createElement("li")
+		list_item1.innerHTML = casual_termekek[1].dizajn_anyag_extrak1
+		let list_item2 = document.createElement("li")
+		list_item2.innerHTML = casual_termekek[1].dizajn_anyag_extrak2
+		let list_item3 = document.createElement("li")
+		list_item3.innerHTML = casual_termekek[1].dizajn_anyag_extrak3
+		let list_item4 = document.createElement("li")
+		list_item4.innerHTML = casual_termekek[1].dizajn_anyag_extrak4
+		let list_item5 = document.createElement("li")
+		list_item5.innerHTML = casual_termekek[1].dizajn_anyag_extrak5
+		let list_item6 = document.createElement("li")
+		list_item6.innerHTML = casual_termekek[1].dizajn_anyag_extrak6
+		let list_item7 = document.createElement("li")
+		list_item7.innerHTML = casual_termekek[1].dizajn_anyag_extrak7
+		let list_item8 = document.createElement("li")
+		list_item8.innerHTML = casual_termekek[1].dizajn_anyag_extrak8
+		let list_item9 = document.createElement("li")
+		list_item9.innerHTML = casual_termekek[1].dizajn_anyag_extrak9
+
+		un_list_des.appendChild(list_item1)
+		un_list_des.appendChild(list_item2)
+		un_list_des.appendChild(list_item3)
+		un_list_des.appendChild(list_item4)
+		un_list_des.appendChild(list_item5)
+		un_list_des.appendChild(list_item6)
+		un_list_des.appendChild(list_item7)
+		un_list_des.appendChild(list_item8)
+		un_list_des.appendChild(list_item9)
+
+		let list_item10 = document.createElement("li")
+		list_item10.innerHTML = casual_termekek[1].meret_szabas1
+		let list_item11 = document.createElement("li")
+		list_item11.innerHTML = casual_termekek[1].meret_szabas2
+		let list_item12 = document.createElement("li")
+		list_item12.innerHTML = casual_termekek[1].meret_szabas3
+		let list_item13 = document.createElement("li")
+		list_item13.innerHTML = casual_termekek[1].meret_szabas4
+
+		un_list_meret.appendChild(list_item10)
+		un_list_meret.appendChild(list_item11)
+		un_list_meret.appendChild(list_item12)
+		un_list_meret.appendChild(list_item13)
+
+		main_image.src="../kepek/white-t-m.png"
+	}
+
+	else if (productSzam == 9) {
+		oldalcim.innerHTML = "Ladies Hat"
+		fejlec_cim.innerHTML = "Home / Products / Casual / Ladies Hat"
+
+		document.getElementById("product_nev").innerHTML = casual_termekek[2].nev;
+
+		document.getElementById("product_ar").innerHTML = casual_termekek[2].sale_price;
+		document.getElementById("learazott").innerHTML = casual_termekek[2].real_price;
+		
+		let list_item1 = document.createElement("li")
+		list_item1.innerHTML = casual_termekek[2].dizajn_anyag_extrak1
+		let list_item2 = document.createElement("li")
+		list_item2.innerHTML = casual_termekek[2].dizajn_anyag_extrak2
+		let list_item3 = document.createElement("li")
+		list_item3.innerHTML = casual_termekek[2].dizajn_anyag_extrak3
+
+		un_list_des.appendChild(list_item1)
+		un_list_des.appendChild(list_item2)
+		un_list_des.appendChild(list_item3)
+
+		let list_item4 = document.createElement("li")
+		list_item4.innerHTML = casual_termekek[2].meret_szabas1
+
+		un_list_meret.appendChild(list_item4)
+
+		main_image.src="../kepek/hat-f.png"
+	}
+
+	else if (productSzam == 10) {
+		oldalcim.innerHTML = "Collar T-Shirt"
+		fejlec_cim.innerHTML = "Home / Products / Casual / Collar T-Shirt"
+
+		document.getElementById("product_nev").innerHTML = casual_termekek[3].nev;
+
+		document.getElementById("product_ar").innerHTML = casual_termekek[3].sale_price;
+		document.getElementById("learazott").innerHTML = casual_termekek[3].real_price;
+		
+		let list_item1 = document.createElement("li")
+		list_item1.innerHTML = casual_termekek[3].dizajn_anyag_extrak1
+		let list_item2 = document.createElement("li")
+		list_item2.innerHTML = casual_termekek[3].dizajn_anyag_extrak2
+		let list_item3 = document.createElement("li")
+		list_item3.innerHTML = casual_termekek[3].dizajn_anyag_extrak3
+		let list_item4 = document.createElement("li")
+		list_item4.innerHTML = casual_termekek[3].dizajn_anyag_extrak4
+		let list_item5 = document.createElement("li")
+		list_item5.innerHTML = casual_termekek[3].dizajn_anyag_extrak5
+		let list_item6 = document.createElement("li")
+		list_item6.innerHTML = casual_termekek[3].dizajn_anyag_extrak6
+		let list_item7 = document.createElement("li")
+		list_item7.innerHTML = casual_termekek[3].dizajn_anyag_extrak7
+		let list_item8 = document.createElement("li")
+		list_item8.innerHTML = casual_termekek[3].dizajn_anyag_extrak8
+		let list_item9 = document.createElement("li")
+		list_item9.innerHTML = casual_termekek[3].dizajn_anyag_extrak9
+
+		un_list_des.appendChild(list_item1)
+		un_list_des.appendChild(list_item2)
+		un_list_des.appendChild(list_item3)
+		un_list_des.appendChild(list_item4)
+		un_list_des.appendChild(list_item5)
+		un_list_des.appendChild(list_item6)
+		un_list_des.appendChild(list_item7)
+		un_list_des.appendChild(list_item8)
+		un_list_des.appendChild(list_item9)
+
+		let list_item10 = document.createElement("li")
+		list_item10.innerHTML = casual_termekek[3].meret_szabas1
+
+		un_list_meret.appendChild(list_item10)
+
+		main_image.src="../kepek/leather-jacket-m.png"
+	}
+
+	else if (productSzam == 11) {
+		oldalcim.innerHTML = "Printed Tops"
+		fejlec_cim.innerHTML = "Home / Products / Casual / Printed Tops"
+
+		document.getElementById("product_nev").innerHTML = casual_termekek[4].nev;
+
+		document.getElementById("product_ar").innerHTML = casual_termekek[4].price;
+		
+		let list_item1 = document.createElement("li")
+		list_item1.innerHTML = casual_termekek[4].dizajn_anyag_extrak1
+		let list_item2 = document.createElement("li")
+		list_item2.innerHTML = casual_termekek[4].dizajn_anyag_extrak2
+		let list_item3 = document.createElement("li")
+		list_item3.innerHTML = casual_termekek[4].dizajn_anyag_extrak3
+		let list_item4 = document.createElement("li")
+		list_item4.innerHTML = casual_termekek[4].dizajn_anyag_extrak4
+		let list_item5 = document.createElement("li")
+		list_item5.innerHTML = casual_termekek[4].dizajn_anyag_extrak5
+		let list_item6 = document.createElement("li")
+		list_item6.innerHTML = casual_termekek[4].dizajn_anyag_extrak6
+		let list_item7 = document.createElement("li")
+		list_item7.innerHTML = casual_termekek[4].dizajn_anyag_extrak7
+		let list_item8 = document.createElement("li")
+		list_item8.innerHTML = casual_termekek[4].dizajn_anyag_extrak8
+		let list_item9 = document.createElement("li")
+		list_item9.innerHTML = casual_termekek[4].dizajn_anyag_extrak9
+
+		un_list_des.appendChild(list_item1)
+		un_list_des.appendChild(list_item2)
+		un_list_des.appendChild(list_item3)
+		un_list_des.appendChild(list_item4)
+		un_list_des.appendChild(list_item5)
+		un_list_des.appendChild(list_item6)
+		un_list_des.appendChild(list_item7)
+		un_list_des.appendChild(list_item8)
+		un_list_des.appendChild(list_item9)
+
+		let list_item10 = document.createElement("li")
+		list_item10.innerHTML = casual_termekek[4].meret_szabas1
+		let list_item11 = document.createElement("li")
+		list_item11.innerHTML = casual_termekek[4].meret_szabas2
+		let list_item12 = document.createElement("li")
+		list_item12.innerHTML = casual_termekek[4].meret_szabas3
+		let list_item13 = document.createElement("li")
+		list_item13.innerHTML = casual_termekek[4].meret_szabas4
+
+		un_list_meret.appendChild(list_item10)
+		un_list_meret.appendChild(list_item11)
+		un_list_meret.appendChild(list_item12)
+		un_list_meret.appendChild(list_item13)
+
+		main_image.src="../kepek/patterned-dress-f.png"
+	}
+
+	else if (productSzam == 12) {
+		oldalcim.innerHTML = "Women Tops"
+		fejlec_cim.innerHTML = "Home / Products / Casual / Women Tops"
+
+		document.getElementById("product_nev").innerHTML = casual_termekek[5].nev;
+
+		document.getElementById("product_ar").innerHTML = casual_termekek[5].price;
+		
+		let list_item1 = document.createElement("li")
+		list_item1.innerHTML = casual_termekek[5].dizajn_anyag_extrak1
+		let list_item2 = document.createElement("li")
+		list_item2.innerHTML = casual_termekek[5].dizajn_anyag_extrak2
+		let list_item3 = document.createElement("li")
+		list_item3.innerHTML = casual_termekek[5].dizajn_anyag_extrak3
+		let list_item4 = document.createElement("li")
+		list_item4.innerHTML = casual_termekek[5].dizajn_anyag_extrak4
+		let list_item5 = document.createElement("li")
+		list_item5.innerHTML = casual_termekek[5].dizajn_anyag_extrak5
+		let list_item6 = document.createElement("li")
+		list_item6.innerHTML = casual_termekek[5].dizajn_anyag_extrak6
+		let list_item7 = document.createElement("li")
+		list_item7.innerHTML = casual_termekek[5].dizajn_anyag_extrak7
+		let list_item8 = document.createElement("li")
+		list_item8.innerHTML = casual_termekek[5].dizajn_anyag_extrak8
+		let list_item9 = document.createElement("li")
+		list_item9.innerHTML = casual_termekek[5].dizajn_anyag_extrak9
+
+		un_list_des.appendChild(list_item1)
+		un_list_des.appendChild(list_item2)
+		un_list_des.appendChild(list_item3)
+		un_list_des.appendChild(list_item4)
+		un_list_des.appendChild(list_item5)
+		un_list_des.appendChild(list_item6)
+		un_list_des.appendChild(list_item7)
+		un_list_des.appendChild(list_item8)
+		un_list_des.appendChild(list_item9)
+
+		let list_item10 = document.createElement("li")
+		list_item10.innerHTML = casual_termekek[5].meret_szabas1
+		let list_item11 = document.createElement("li")
+		list_item11.innerHTML = casual_termekek[5].meret_szabas2
+		let list_item12 = document.createElement("li")
+		list_item12.innerHTML = casual_termekek[5].meret_szabas3
+		let list_item13 = document.createElement("li")
+		list_item13.innerHTML = casual_termekek[5].meret_szabas4
+
+		un_list_meret.appendChild(list_item10)
+		un_list_meret.appendChild(list_item11)
+		un_list_meret.appendChild(list_item12)
+		un_list_meret.appendChild(list_item13)
+
+		main_image.src="../kepek/black-dress-f.png"
+	}
+
+	//clothing
+	else if (productSzam == 13) {
+		oldalcim.innerHTML = "Men adidas hoodie"
+		fejlec_cim.innerHTML = "Home / Products / Casual / Men adidas hoodie"
+
+		document.getElementById("product_nev").innerHTML = casual_termekek[0].nev;
+
+		document.getElementById("product_ar").innerHTML = casual_termekek[0].sale_price;
+		document.getElementById("learazott").innerHTML = casual_termekek[0].real_price;
+		
+		let list_item1 = document.createElement("li")
+		list_item1.innerHTML = casual_termekek[0].dizajn_anyag_extrak1
+		let list_item2 = document.createElement("li")
+		list_item2.innerHTML = casual_termekek[0].dizajn_anyag_extrak2
+		let list_item3 = document.createElement("li")
+		list_item3.innerHTML = casual_termekek[0].dizajn_anyag_extrak3
+		let list_item4 = document.createElement("li")
+		list_item4.innerHTML = casual_termekek[0].dizajn_anyag_extrak4
+		let list_item5 = document.createElement("li")
+		list_item5.innerHTML = casual_termekek[0].dizajn_anyag_extrak5
+
+		un_list_des.appendChild(list_item1)
+		un_list_des.appendChild(list_item2)
+		un_list_des.appendChild(list_item3)
+		un_list_des.appendChild(list_item4)
+		un_list_des.appendChild(list_item5)
+
+		let list_item6 = document.createElement("li")
+		list_item6.innerHTML = casual_termekek[0].meret_szabas1
+		let list_item7 = document.createElement("li")
+		list_item7.innerHTML = casual_termekek[0].meret_szabas1
+		let list_item8 = document.createElement("li")
+		list_item8.innerHTML = casual_termekek[0].meret_szabas1
+
+		un_list_meret.appendChild(list_item6)
+		un_list_meret.appendChild(list_item7)
+		un_list_meret.appendChild(list_item8)
+
+		main_image.src="../kepek/leather-jacket-m.png"
+	}
+
+	else if (productSzam == 14) {
+
+	}
+
+	else if (productSzam == 15) {
+
+	}
+
+	else if (productSzam == 16) {
+
+	}
+
+	else if (productSzam == 17) {
+
+	}
+
+	else if (productSzam == 18) {
+
 	}
 }
 
@@ -511,9 +809,9 @@ let casual_termekek = [
 		dizajn_anyag_extrak9:"Nyakszalag",
 
 		meret_szabas1:"Csomag: 1 darabos csomag",
-		meret_szabas1:"Ujjhossz: Negyedes ujj",
-		meret_szabas1:"Hossz: Normál hosszúságú",
-		meret_szabas1:"Fazon: normál fazon"
+		meret_szabas2:"Ujjhossz: Negyedes ujj",
+		meret_szabas3:"Hossz: Normál hosszúságú",
+		meret_szabas4:"Fazon: normál fazon"
 	},
 
 	//3
@@ -531,7 +829,7 @@ let casual_termekek = [
 
 	//4
 	{
-		nev:"Gentlemen's Casual T-Shirt",
+		nev:"Black Maze Jacket 'Berwick'",
 		real_price:"$100.00",
 		sale_price:"$50.00",
 
@@ -588,6 +886,50 @@ let casual_termekek = [
 		meret_szabas2:"Hossz: Hosszú/maxi",
 		meret_szabas3:"Fazon: normál fazon",
 		meret_szabas4:"Vágás: Beépített"
+	},
+]
+
+let clothing_termekek = [
+	//1
+	{
+		nev:"Men adidas workout hoodie",
+		real_price:"$35.00",
+		sale_price:"$19.00",
+
+		dizajn_anyag_extrak1:"Univerzális színek",
+		dizajn_anyag_extrak2:"Pamut",
+		dizajn_anyag_extrak3:"Kapucni",
+		dizajn_anyag_extrak4:"Címke",
+		dizajn_anyag_extrak5:"Ton inTon tűzések",
+
+		meret_szabas1:"Csomag: 1 darabos csomag",
+		meret_szabas2:"Hossz: Normál hosszúságú",
+		meret_szabas3:"Fazon: normál fazon"
+	},
+
+	//2
+	{
+		
+	},
+
+	//3
+	{
+		
+	},
+
+	//4
+	{
+		
+	},
+
+	//5
+	{
+		
+	},
+
+	//6
+	{
+		
 	},
 ]
 
