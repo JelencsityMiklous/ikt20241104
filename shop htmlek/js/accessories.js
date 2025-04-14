a8991c53f86985f50e4e6470136a4418135cb843
OhradkaGergo <ohradkagergo@turr.hu>
2025-04-14 20:18:52 +0200

    TopButton beillesztése a kimaradt oldalakra

--- shop htmlek/js/accessories.js
+++ shop htmlek/js/accessories.js
@@ -2,6 +2,24 @@ function megszerezAdat(productSzam) {
     window.open(`ruhadarab.html?id=${productSzam}`, '_blank'); 
 }
 
+let mybutton = document.getElementById("topgomb");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	}
+	else {
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0; // Safari
+	document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
+
 /* Objektum a termékekről */
 
 let termekek = [
